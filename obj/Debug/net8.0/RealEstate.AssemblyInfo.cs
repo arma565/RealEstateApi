7302f350de8839d54c1f3469868118050214ebbe
Reza <rezagorji68@yahoo.com>
2024-11-11 19:43:07 +0330

    update extention

--- obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fca681d9119b9dedf5b19a973c814e7c7981dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
