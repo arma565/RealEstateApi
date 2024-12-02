bd94450c384d5dff537da5ba0a62bb2162f5a69e
Reza <rezagorji68@yahoo.com>
2024-12-02 22:08:38 +0330

    Delete private image

--- obj/Debug/net9.0/RealEstate.AssemblyInfo.cs
+++ obj/Debug/net9.0/RealEstate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b85b51777456be9827a05806783dccf0ea783c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e5c70edefcca5ca522bf9ad91864c2f7fb5b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
