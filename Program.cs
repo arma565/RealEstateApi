cf0fea6ccc8205c122ff07cd6984519ed21852fe
Reza <rezagorji68@yahoo.com>
2024-12-14 19:17:34 +0330

    update extention and

--- Program.cs
+++ Program.cs
@@ -4,23 +4,43 @@ using Microsoft.EntityFrameworkCore;
 
 var MyAllowSpecificOrigins = "_estatePolicy";
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options=>{
-    options.AddPolicy(MyAllowSpecificOrigins , policy =>{
-        policy.WithOrigins("http://localhost:5068").AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
-    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:5068")
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod();
+        }
+    );
 });
+
 // Add services to the container.
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PropertyConnection")));      
-builder.Services.AddIdentity<UserProfileIdentity , IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PropertyConnection"))
+);
+builder
+    .Services.AddIdentity<UserProfileIdentity, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<RepositoryService>();
 builder.Services.AddScoped<PasswordHelper>();
-builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddHttpClient();
+builder
+    .Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
