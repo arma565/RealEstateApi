6de86e522e505d4ccdb08845945c0ce35929cfb2
Reza <rezagorji68@yahoo.com>
2025-02-19 19:57:07 +0330

    Update extentions

--- obj/Debug/net9.0/RealEstate.AssemblyInfo.cs
+++ obj/Debug/net9.0/RealEstate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8a0fb8db5158fbe3ea88c26854b7df39abad54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e9038cedcb12e1c52d3611cc03d10c19c7b192")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
