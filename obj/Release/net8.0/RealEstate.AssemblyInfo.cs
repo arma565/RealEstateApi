b3a82875dbd0b2c4b1a5e47d2e768b720b7a0388
Reza <rezagorji68@gmail.com>
2025-05-30 22:01:20 +0330

    fix bugs

--- obj/Release/net8.0/RealEstate.AssemblyInfo.cs
+++ obj/Release/net8.0/RealEstate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb352de2d1e2aed6792cd88c9ddd990b874829a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d2c2171d4451b5bca2e5b2fec4d512dbd6c4de")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
